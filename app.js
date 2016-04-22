709c27ae80526aa89f2759aa5793373eb9bc41dc
Hiram Huang <chown9835@gmail.com>
2016-04-22 22:16:08 +0000

    add CORS

--- app.js
+++ app.js
@@ -5,10 +5,20 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+const Account = require("./models/account");
+const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
+
+const routes = require('./routes/index');
+const auth = require("./routes/auth");
+const config = require("./config");
 
 var app = express();
+
+// use q for mongoose promise
+var mongoose = require("mongoose");
+mongoose.Promise = require('q').Promise;
+mongoose.connect(config.dbpath);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,18 +32,54 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+config.passport = passport;
+
+// Passport middleware
+passport.use(new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'hashed_password'
+  },
+  function(username, password, done) {
+    Account.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  Account.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+// CORS
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+ 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/auth', auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  res.status(404);
 });
 
 // error handlers
-
+/*
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
@@ -55,6 +101,6 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
+*/
 
 module.exports = app;
